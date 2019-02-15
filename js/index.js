c202c2363a95ed8e1976282a170f223c969dfbce
Levi-Huynh <Levi_huynh@yahoo.com>
2019-02-14 20:37:55 -0800

    services, html desktop and html mobile

--- js/index.js
+++ js/index.js
@@ -1,1 +1,56 @@
 // JS goes here
+
+class TabLink {
+
+    constructor(element) {
+        this.element= element;
+         this.data= element.dataset.tab;
+        console.log(this.data);
+         this.elementItem = document.querySelector(`.tabs-item[data-tab='${element.dataset.tab}']`);
+        //create new instances/objects with the right tab item attributes
+        this.tabItem= new TabItem(this.elementItem);
+        console.log(this.tabItem);
+        element.addEventListener('click', () => this.select());
+
+    };
+
+    select() {
+        const links = document.querySelectorAll('tabs-link');
+        // const links = document.querySelectorAll('.tabs-link');
+        links.forEach(link => link.style.backgroundColor='white');
+        //all links has tabs-link-selected removed from it 
+        // Array.from(links).forEach(link => link.classList.remove('tabs-link-selected'));
+        links.forEach(link => link.classList.remove('tabs-link-selected'));
+        this.element.classList.add('tabs-link-selected');
+        this.element.style.backgroundColor='#609CB7';
+        //this.tabItem= new TabItem(this.elementItem)(*corresponding/matching tabItem 
+        //goes through the selection method Each parent construct deals with a seperate Main element in HTML)
+        links.forEach(link=> link.style.backgroundColor="white");
+        this.tabItem.select();
+    }
+
+}
+
+class TabItem {
+    constructor(element){
+
+        this.element= element;
+    }
+
+    select() {
+        // const items = document.getElementsByClassName('tabs-item');
+        const items= document.querySelectorAll('.tabs-item');
+        console.log(items);
+            //all tabs-item has class removed from it 
+            items.forEach(item => item.classList.remove('tabs-item-selected'));
+    // Array.from(items).forEach(item => item.classlist.remove('tabs-item-selected'));
+           //variable passed through & binded to TabItem has the class added to it.  
+        this.element.classList.add('tabs-item-selected');
+    }
+
+}
+
+//makes the above true for every link created
+const links =document.querySelectorAll('.tabs-link');
+console.log(links);
+links.forEach( element => new TabLink(element));
